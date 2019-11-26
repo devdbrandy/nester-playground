c3b605636da49c13cdc42fb45f04809963597b69
Harry Dbrandy <devdbrandy@gmail.com>
2019-11-26 10:32:39 +0100

    API crud operation with in-memory data storage 🚧

---
+++ src/products/products.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { Product } from 'src/models/product.model';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+
+  constructor(private productsService: ProductsService) {}
+
+  @Post()
+  addProduct(
+    @Body('title') title: string,
+    @Body('description') description: string,
+    @Body('price') price: number,
+  ): { id: string } {
+    const productId = this.productsService.addProduct(title, description, price);
+    return { id: productId };
+  }
+
+  @Get()
+  getProducts(): { products: Product[] } {
+    const products = this.productsService.getProducts();
+    return { products };
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') id: string): { product: Product } {
+    const product = this.productsService.getProduct(id);
+    return { product };
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body() body: { title: string, description: string, price: number },
+  ): { message: string } {
+    this.productsService.updateProduct(id, body);
+    return { message: 'Product successfully updated.' };
+  }
+
+  @Delete(':id')
+  destroyProduct(@Param('id') id: string): { message: string } {
+    this.productsService.destroyProduct(id);
+    return { message: 'Product successfully removed' };
+  }
+}
