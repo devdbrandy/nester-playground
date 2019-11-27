2dbf4955ff05ee575a512b0e8329e4de7f408d3f
Harry Dbrandy <devdbrandy@gmail.com>
2019-11-27 14:38:00 +0100

    configure openAPI (fka swagger) 📝

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest Products')
+    .setDescription('The Nest Products API description')
+    .setVersion('1.0')
+    .addTag('nest')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
